dbf353a12420227f08df316bd1f7a857b2d8f420
David Steiner <david1.steiner@gmail.com>
2024-05-02 14:39:57 +0200

    added boilerplate and brainstorming in index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//Check if user has localstorage todoList
+//  if so Load localstorage todoList
+//else render home page
+
+//Create items through classes
+//Create projects through classes
+//Store items in categories
+//      item contains:
+//          {title:str, description:str, dueDate:new Date, priority:1-5, type:checklist vs Notes ????,}
+//      category contains:
+//          {title:str, items:{}, priority:1-5,}
+
+//createCategory(...args)
+//getAllCategories()
+//editCategory()
+//deleteCategory()
+
+//createItem(...args)
+//getCategoryItems(category.title)
+//editItem()
+//deleteItem()
+//markItemCompleted()
